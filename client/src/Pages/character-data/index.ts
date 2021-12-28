2babf3cca7d43e48e9c154ba07a641fa39309318
Xendergo <not-my-email@example.com>
2021-12-28 07:28:36 -0500

    Improve scrolling

--- client/src/Pages/character-data/index.ts
+++ client/src/Pages/character-data/index.ts
@@ -5,14 +5,28 @@ const app = new App({
     target: document.getElementById("app")!,
 })
 
+let displacement: number | null = null
+let displacementTimeout: number | undefined = undefined
+
 window.addEventListener("wheel", e => {
     e.preventDefault()
 
-    scroll({
-        top: 0,
-        left: document.documentElement.scrollLeft - e.deltaY + e.deltaX,
-        behavior: "smooth",
-    })
+    if (displacement === null) {
+        displacement = document.documentElement.scrollLeft + e.deltaY + e.deltaX
+    } else {
+        displacement = displacement + e.deltaY + e.deltaX
+    }
+
+    clearTimeout(displacementTimeout)
+    displacementTimeout = setTimeout(() => {
+        displacement = null
+    }, 500) as unknown as number
 })
+
+setInterval(() => {
+    if (displacement !== null) {
+        scrollBy((displacement - document.documentElement.scrollLeft) * 0.15, 0)
+    }
+}, 1000 / 60)
 
 export default app
